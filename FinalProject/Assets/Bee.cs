0f1e77af4179d6a9c043b6aee5e8595f40484d50
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2024-12-01 13:44:26 -0600

    Maskable danger zones

--- FinalProject/Assets/Bee.cs
+++ FinalProject/Assets/Bee.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Bee : MonoBehaviour
 {
+    // Helps smooth movement 
     const float MOVE_SMOOTH_FACTOR = 3f;
 
     public bool followMode = false;
     private Rigidbody2D rb;
+    public bool inDangerZone = false;
+    public bool inDangerMask = false;
 
     private void Awake()
     {
@@ -29,15 +32,57 @@ public class Bee : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (inDangerMask && inDangerZone)
+        {
+            // Die
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+
+        // Stay still until moused over
+        if (!followMode)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.up, 0f, LayerMask.GetMask("Bee"));
+
+            if (hit)
+            {
+                followMode = true;
+            }
+        }
+        
+        // Move to follow mouse
         if (followMode && (mousePos - rb.position).magnitude > 0.1)
         {
             // Look at mouse
             transform.up = mousePos - rb.position;
+            // Move toward mouse with collisions and slight smoothing
             rb.velocity = (mousePos - rb.position) / Time.fixedDeltaTime / MOVE_SMOOTH_FACTOR;
         }
         else
         {
+            // Stay still if mouse hasn't moved much
             rb.velocity = Vector2.zero;
+        }
+
+        
+
+        // OnTriggerStay will call after this
+        inDangerZone = false;
+        inDangerMask = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<DangerZone>() != null)
+        {
+            inDangerZone = true;
+        }
+        if (collision.gameObject.GetComponent<DangerMask>() != null)
+        {
+            inDangerMask = true;
         }
     }
 }
