f18ce47810870a29bfd0c3c5b1096d36166b2ca9
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2024-12-10 00:24:15 -0600

    so graphics wow

--- FinalProject/Assets/Scripts/Balloon.cs
+++ FinalProject/Assets/Scripts/Balloon.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
+    public ParticleSystem ps;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Bee>() != null)
         {
             GameManager.S.BallonDestroyed();
+            var go = GameObject.Instantiate(ps.gameObject);
+            go.transform.position = transform.position;
+            Destroy(go, 1f);
             Destroy(gameObject);
         }
     }
